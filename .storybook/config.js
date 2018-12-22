d2e7dda4f47307164f8aee07134c654870231195
Farhad Yasir <farhad.yasir.nb@gmail.com>
2018-12-22 16:57:18 +1100

    Feature/remove packages (#2)
    * [cleanup] remove unused packages from the library
    
    * [feat] restructure storybook stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@storybook/react';
-// import '../.storybook/styles/base.scss';
 
-const req = require.context('../src/stories', true, /.stories.ts*/);
+const req = require.context('../src/stories', true, /.ts*/);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
--- src/stories/Default.tsx
+++ src/stories/Default.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs/react';
+import { withKnobs } from '@storybook/addon-knobs/react';
 import { withInfo } from '@storybook/addon-info';
 
 import FocusOnScroll from '../components/FocusOnScroll';
@@ -47,45 +47,5 @@ stories.add('Default (with options)', withInfo()(() => {
 }));
 
 
-stories.add('Small sized sections (with options)', () => {
-  // const defaultProps = {
-  //   focusOn: text('focusOn: ', '1'),
-  // };
-  // const divProps = {
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // };
-  return (
-    <FocusOnScroll
-      // {...defaultProps}
-    >
-      <div  style={{ height: 30, background: '#00E4E4' }}>
-        <div  color="white">0</div>
-      </div>
-      <div  style={{ height: 50, background: '#8DE2E0' }}>
-        <div >1</div>
-        <input
-          name={text('name: ', 'testField')}
-          id={text('id: ', '')}
-          placeholder={text('placeholder: ', 'Enter your test field')}
-        />
-      </div>
-      <div  style={{ height: 600, background: '#E40000' }}>
-        <div  color="white">2</div>
-      </div>
-      <div  style={{ height: 200, background: '#009100' }}>
-        <div  color="white">3</div>
-      </div>
-      <div  style={{ height: 200, background: '#ED710B' }}>
-        <div  color="white">4</div>
-      </div>
-      <div  style={{ height: 30, background: '#F9F3E9' }}>
-        <div >5</div>
-      </div>
-      <div  style={{ height: 30, background: '#A9A379' }}>
-        <div >6</div>
-      </div>
-    </FocusOnScroll>
-  );
-});
+
 
