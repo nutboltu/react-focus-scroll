6fe2cff89d41f0cb6095635e13c716e33e9d0bc6
Farhad Yasir <farhad.yasir.nb@gmail.com>
2018-12-25 12:23:32 +1100

    #f

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const CompressionPlugin = require('compression-webpack-plugin');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const getCommonConfig = require('./common.config');
 
 module.exports = merge(getCommonConfig('production'), {
@@ -18,15 +16,6 @@ module.exports = merge(getCommonConfig('production'), {
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
-    // new UglifyJsPlugin({
-    //   cache: true,
-    //   parallel: true,
-    //   uglifyOptions: { ecma: 8 },
-    // }),
-    // new CompressionPlugin({
-    //   test: /\.js$/,
-    //   minRatio: 0.8,
-    // }),
   ],
   externals: [
     'react',
