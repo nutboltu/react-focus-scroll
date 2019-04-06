e9f5a6a176e279a006b5c4f1f375ea27cef90bfa
Farhad Yasir <farhad.yasir.nb@gmail.com>
2019-04-06 18:06:22 +1100

    feat(improve-perf): Removing all unnecessary calculation from findFocus function

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,43 +1,27 @@
-import { IFocusArea, ChildrenArray } from '../@types';
-
-export const findFocusArea = (childrenArray: any[]): IFocusArea => {
-  if (!childrenArray.length) {
-    return { top: 0, bottom: 0 };
-  }
-
-  const bottom = window.innerHeight * 0.50;
-  const minHeight = childrenArray.reduce((acc, cur) =>
-    Math.min(cur.clientHeight, acc), bottom);
-
-  return {
-    top: bottom - (minHeight * 0.50),
-    bottom,
-  };
-};
-
-export const findFocusElement = (focusArea: IFocusArea, childrenArray: ChildrenArray) => {
-  const windowHeight = document.body.offsetHeight - window.innerHeight;
-
-  if (childrenArray.length === 0) {
+export const findFocusElement = () => {
+  const sectionNodes = document.getElementsByClassName('rfs-section');
+  const sections = Array.prototype.slice.call(sectionNodes);
+  const totalHeight = document.body.offsetHeight - window.innerHeight;
+  if (sections.length === 0) {
     return -1;
   }
   if (window.scrollY === 0) {
     return 0;
   }
-  if (window.scrollY >= windowHeight) {
-    return childrenArray.length - 1;
+  if (window.scrollY >= totalHeight) {
+    return sections.length - 1;
   }
 
-  const focusTop = focusArea.top + window.scrollY;
-  const focusBottom = focusArea.bottom + window.scrollY;
+  const focusPoint = window.innerHeight / 2;
 
-  return childrenArray.reduce((acc, element, index) => {
-    const top = element.getBoundingClientRect().top + window.pageYOffset;
-
-    const bottom = top + element.clientHeight;
-    const isInfocusArea = !(top > focusBottom || bottom < focusTop);
-    return isInfocusArea ? index : acc;
-  }, 0);
+  for (let i = 0 ; i < sections.length; i++) {
+    const top = sections[i].getBoundingClientRect().top;
+    const bottom = top + sections[i].getBoundingClientRect().height;
+    if (!(top > focusPoint || bottom < focusPoint)) {
+      return i;
+    }
+  }
+  return -1;
 };
 
 export const debounce = (fn: any, wait: number) => {
@@ -57,8 +41,8 @@ export const debounce = (fn: any, wait: number) => {
 
 export const throttle = (fn: any, wait: number) => {
   let throttled = false;
-  // tslint:disable-next-line:one-variable-per-declaration
-  let context: any, args: any;
+  let context: any;
+  let args: any;
   const resolver = function() {
     if (throttled) {
       args = arguments;
